f847ef7991c15d76c64d6656aba29624fff04220
samuelarredondo <arredondosamuel0@gmail.com>
2025-03-04 18:30:52 -0500

    adicione la interfaz web

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR Johan Taborda Hola
+          Edit <code>src/App.jsx</code> Rama Samuel esta melaa
         </p>
       </div>
       <p className="read-the-docs">
